a1bd73b335178469d394ffc38cb4431c5872dc25
ALASHI1 <alashimuyiwa@gmail.com>
2022-01-07 16:25:29 +0100

    html fix

--- resume/views.py
+++ resume/views.py
@@ -9,7 +9,7 @@ def index(request):
 			form.save()
 			return render(request,'thanks.html',{'form':form})
 	else:
-		form = ContactForm()
+			form = ContactForm()
   
   
 	return render(request,'index.html',{'form':form})
