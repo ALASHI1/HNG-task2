d0696d09ba178b74900a737133b8515473d62efb
ALASHI1 <alashimuyiwa@gmail.com>
2023-09-13 18:44:21 +0100

    xframes

--- myresume/settings.py
+++ myresume/settings.py
@@ -156,3 +156,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+X_FRAME_OPTIONS = "SAMEORIGIN"
